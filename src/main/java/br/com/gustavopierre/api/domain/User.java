faeac4f78a337f5563e6995a6ab4801b2b75f9f9
Gustavo Pierre <gustavopierre@gmail.com>
2022-06-16 18:22:32 +0100

    User Class

---
+++ src/main/java/br/com/gustavopierre/api/domain/User.java
@@ -0,0 +1,25 @@
+package br.com.gustavopierre.api.domain;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
