f3d7e0e5808f5a46ceb9800d3c4fac368661d1ae
Gustavo Pierre <gustavopierre@gmail.com>
2022-06-18 00:50:24 +0100

    Exception for existing email

--- src/main/java/br/com/gustavopierre/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/gustavopierre/api/services/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import br.com.gustavopierre.api.domain.User;
 import br.com.gustavopierre.api.domain.dto.UserDTO;
 import br.com.gustavopierre.api.repositories.UserRepository;
 import br.com.gustavopierre.api.services.UserService;
+import br.com.gustavopierre.api.services.exceptions.DataIntegrityViolationException;
 import br.com.gustavopierre.api.services.exceptions.ObjectNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +33,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User create(UserDTO obj) {
+        findByEmail(obj);
         return repository.save(mapper.map(obj, User.class));
+    }
+
+    private void findByEmail(UserDTO obj){
+        Optional<User> user = repository.findByEmail(obj.getEmail());
+        if (user.isPresent()){
+            throw new DataIntegrityViolationException("E-mail already exists");
+        }
     }
 }
